0ea028cc6302d430b840f3a63e6d3b601caeb485
Shay Zeitune <shayzeitune@gmail.com>
2025-03-19 21:46:04 +0200

    changed getRainDate() to getForecastDate(). the function is now  extracting data from the forecast of the target location and assign it to objects then the objects to array.

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,71 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.sql.SQLOutput;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Main {
     static String targetLocation = "Jerusalem"; // Change this to filter by a different location
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        LocalDate localDate = LocalDate.now();
-        LocalTime now = LocalTime.now();
-        System.out.println(now);
-        System.out.println(isRain(getForecastData()));
+
+        // Get current time in Israel time zone
+        LocalDateTime now = LocalDateTime.now();
+        turnToUnix(now);
+        String timeString = ""; // Example time in Israel time (summer)
+        if (!whenIsRain(getForecastData()).equals("0")) {
+            // if there is rain in the upcoming days print the time when it is going to rain
+            System.out.println(" it is going to rain rain in " + targetLocation + "at: " + whenIsRain(getForecastData()));
+            timeString = whenIsRain(getForecastData());
+        }
+        else{
+            System.out.println("there is no rain soon");
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // Parse string to LocalDateTime
+        LocalDateTime dateTime = LocalDateTime.parse(timeString, formatter);
+        turnToUnix(dateTime);
+
+        if(turnToUnix(dateTime)< turnToUnix(now)){
+            System.out.println(" bhahahahahahahaahahahahahahahajjj");
+        }
+
+        // Convert to Israel time zone
+//        long unixTimestamp = dateTime.atZone(ZoneId.of("Asia/Jerusalem")).toEpochSecond();
+
+//        System.out.println("Unix Timestamp (Israel Time converted to UTC): " + unixTimestamp);
+
+
+//        ZonedDateTime israelTime = now.atZone(ZoneId.of("Asia/Jerusalem")); // Convert to Israel time
+//        ZonedDateTime utcTime = israelTime.withZoneSameInstant(ZoneOffset.UTC); // Convert to UTC
+//        long unixTimestamp = utcTime.toEpochSecond();
+//        System.out.println("Unix Timestamp (UTC): " + unixTimestamp);
+//        System.out.println("Israel Time: " + israelTime);
+//        System.out.println("UTC Time: " + utcTime);
+//        String timeString = whenIsRain(getForecastData()); // Example time string
+//
+//        // Define the format that matches your string
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        // Parse the string into LocalDateTime
+//        LocalDateTime dateTime = LocalDateTime.parse(timeString, formatter);
+//        ZonedDateTime rainTime = dateTime.atZone(ZoneId.of("Asia/Jerusalem")); // Convert to Israel time
+//        ZonedDateTime utcRainTime = israelTime.withZoneSameInstant(ZoneOffset.UTC); // Convert to UTC
+//        long unixRainTimestamp = utcTime.toEpochSecond();
+
+
+
+
+
+
+        // Convert to Unix timestamp (seconds)
+
+
 
 
 /// Print the data array
-        System.out.println(" Forecasts data for " + targetLocation + ": " + getForecastData());
+//        System.out.println(" Forecasts data for " + targetLocation + ": " + getForecastData());
 ///   if there is a rain in somewhere in the future in the location chosen
 //        if (isRain(getForecastData())){
 //            System.out.println("hi");
@@ -66,20 +114,34 @@ public class Main {
                     forecastsData.add(forecast1);
 //                    rainData.add(Double.parseDouble(rainValue));
                 }
-                break; // Exit the loop once the location is found
+                break; // Exit the loopi once the location is found
             }
         }
         return forecastsData;
 
     }
 
-    public static Boolean isRain(ArrayList<Forecast> forecastData){
+    public static String whenIsRain(ArrayList<Forecast> forecastData){
+        // check if there is rain during the upcoming days in the relevant location.
        for (int i = 0; i< forecastData.size(); i++)
            if (forecastData.get(i).rain > 0){
-               return true;
+               return forecastData.get(i).time;
            }
 
-        return false;
+        return "0";
+    }
+
+    public static long turnToUnix(LocalDateTime time)
+            /// turn the time into unix format.
+    {
+        ZonedDateTime israelTime = time.atZone(ZoneId.of("Asia/Jerusalem")); // Convert to Israel time
+        ZonedDateTime utcTime = israelTime.withZoneSameInstant(ZoneOffset.UTC); // Convert to UTC
+        long unixTimestamp = utcTime.toEpochSecond();
+       System.out.println("Unix Timestamp (UTC): " + unixTimestamp);
+       System.out.println("Israel Time: " + israelTime);
+       System.out.println("UTC Time: " + utcTime);
+       return unixTimestamp;
+
     }
 
 }
